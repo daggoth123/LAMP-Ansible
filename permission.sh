b1ccf3f0775a82ccb9ac6847f009b638c1ae4248
root <root@core1.davaitest.com>
2022-08-05 13:36:01 -0400

    revisions

--- permission.sh
+++ permission.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 find /etc/ansible -type f -exec chmod 644 {} \;
 find /etc/ansible -type d -exec chmod 755 {} \;
+chmod 755 /etc/ansible/roles/httpd_config/templates/sed.sh /etc/ansible/roles/httpd_config/templates/wrapper.sh
 chown -R root. /etc/ansible/
